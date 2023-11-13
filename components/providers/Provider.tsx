7a7fba936136996e0a07e810be1cad17502fb1e1
dvv <dvv0714@gmail.com>
2023-11-13 10:16:10 -0500

    update environment

--- components/providers/Provider.tsx
+++ components/providers/Provider.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: React.ReactNode }
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://commerce-next-ashen-eight.vercel.app/stripe/success"
+      cancelUrl="https://commerce-next-ashen-eight.vercel.app/stripe/error"
       currency="CAD"
       billingAddressCollection={false}
       shouldPersist={true}
